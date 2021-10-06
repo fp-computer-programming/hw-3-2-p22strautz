02168925847f8102498ecaf89df07fd0babc9d6d
p22strautz <p22strautz@fairfieldprep.org>
2021-10-06 10:41:50 -0400

    hw3-2 final

--- hw3-2-2.py
+++ hw3-2-2.py
@@ -1,1 +1,18 @@
 # Author: SCT (ADMG) 9/29/21
+
+Height = int(input("What is your height?\n"))
+
+Weight = int(input("What is your weight?\n"))
+
+BMInum = Weight / (Height ** 2)
+
+if BMInum < 19:
+    print("You are underweight")
+elif BMInum < 25:
+    print("You are Healthy")
+elif BMInum < 30:
+    print("You are overweight")
+elif BMInum < 40:
+    print("You are obese")
+elif BMInum >= 40:
+    print("You are extremely obese")
